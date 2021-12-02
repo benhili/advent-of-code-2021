849e25785949cb054edfdec1aae9117004d6eb9f
benhili <ben.hili1994@gmail.com>
2021-12-02 15:36:16 +1100

    Add solution for day1 part 2

--- day1/day1.clj
+++ day1/day1.clj
@@ -2,7 +2,18 @@
 
 (def depths (map read-string (str/split (slurp "day1/day1-input.txt") #"\n")))
 
-(reduce (fn [count [prev next]]
-          (cond (< prev next) (inc count)
-                :else count))
-        0 (partition 2 1 depths))
+(print "Part 1:"
+       (reduce (fn [count [prev next]]
+                 (cond (< prev next) (inc count)
+                       :else count))
+         0
+         (partition 2 1 depths)))
+
+(def windows (map #(reduce + %) (partition 3 1 depths)))
+
+(print "\nPart 2:"
+       (reduce (fn [count [prev next]]
+                 (cond (< prev next) (inc count)
+                       :else count))
+         0
+         (partition 2 1 windows)))
