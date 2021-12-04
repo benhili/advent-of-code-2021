3be766affafbe2fee51bb1a40802b59eb598ba53
benhili <ben.hili1994@gmail.com>
2021-12-04 17:32:36 +1100

    Add day 2 solution cleanup day1

--- day1/day1.clj
+++ day1/day1.clj
@@ -1,19 +1,22 @@
 (require '[clojure.string :as str])
 
-(def depths (map read-string (str/split (slurp "day1/day1-input.txt") #"\n")))
+(def data
+  (map #(read-string %)
+    (-> (slurp "day1/day1-input.txt")
+        (str/split #"\n"))))
 
-(print "Part 1:"
-       (reduce (fn [count [prev next]]
-                 (cond (< prev next) (inc count)
-                       :else count))
-         0
-         (partition 2 1 depths)))
+(defn count-depth
+  [nums]
+  (reduce (fn [count [prev next]]
+            (cond (< prev next) (inc count)
+                  :else count))
+    0
+    (partition 2 1 nums)))
 
-(def windows (map #(reduce + %) (partition 3 1 depths)))
 
-(print "\nPart 2:"
-       (reduce (fn [count [prev next]]
-                 (cond (< prev next) (inc count)
-                       :else count))
-         0
-         (partition 2 1 windows)))
+(def windows
+  (->> (partition 3 1 data)
+       (map #(reduce + %))))
+
+(print "Part 1:" (count-depth data))
+(print "Part 2:" (count-depth windows))
---
+++ day2/day2.clj
@@ -0,0 +1,34 @@
+(require '[clojure.string :as str])
+
+(def data
+  (->> (str/split (slurp "day2/day2-input.txt") #"\n")
+       (map #(let [[_ direction value] (re-matches #"([a-z]+) (\d)" %)]
+               [(keyword direction) (Integer/parseInt value)]))))
+
+
+
+(defn part-1
+  []
+  (let [[depth position] (reduce (fn [[depth position] [direction value]]
+                                   (case direction
+                                     :forward [depth (+ position value)]
+                                     :down [(+ depth value) position]
+                                     :up [(- depth value) position]))
+                           [0 0]
+                           data)]
+  (* depth position)))
+
+(defn part-2
+  []
+  (let [[depth position] (reduce (fn [[depth position aim] [direction value]]
+                                   (case direction
+                                     :forward [(+ depth (* aim value)) (+ position value) aim]
+                                     :down [depth position (+ aim value)]
+                                     :up [depth position (- aim value)]))
+                           [0 0 0]
+                           data)]
+  (* depth position)))
+
+(print "Part 1" (part-1))
+
+(print "Part 2" (part-2))
