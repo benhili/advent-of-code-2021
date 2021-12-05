87f2c6387fcd86dc742e197511183b8e090a8032
benhili <ben.hili1994@gmail.com>
2021-12-05 19:19:07 +1100

    Add day 3 part 2 solution

--- day3/day3.clj
+++ day3/day3.clj
@@ -1,21 +1,58 @@
 (require '[clojure.string :as str])
 
-(def data
-  (->> (-> (slurp "day3/day3-input.txt")
-           (str/split #"\n"))
-       (map #(str/split % #""))
-       (map #(map #(Long/parseLong %) %))))
+(def input (slurp "day3/day3-input.txt"))
+
+(defn invert [bits] (map #(- 1 %) bits))
+
+(defn ones?
+  [lines pos]
+  (let [chars (map #(nth % pos) lines)
+        {zeros \0, ones \1} (frequencies chars)]
+    (>= ones zeros)))
+
+(defn binary-to-decimal
+  [binary]
+  (-> binary
+      str/join
+      (Long/parseLong 2)))
 
 
-(def gamma-rate
-  (->> data
-       (reduce (fn [total bits] (map + bits total)))
-       (map (fn [num]
-              (cond (< num (/ (count data) 2)) 1
-                    :else 0)))
-       ))
+(defn filter-by-frequency
+  [lines keep-ones keep-zeros]
+  (loop [lines lines
+         pos 0]
+    (if (<= (count lines) 1)
+      lines
+      (let [keep (if (ones? lines pos) keep-ones keep-zeros)]
+        (recur (filter #(= keep (nth % pos)) lines) (inc pos))))))
 
-(def gamma (-> gamma-rate str/join (Long/parseLong 2)))
-(def epsilon (-> (map #(- 1 %) gamma-rate) str/join (Long/parseLong 2)))
+(-> input
+    str/split-lines
+    (filter-by-frequency \0 \1)
+    binary-to-decimal)
 
-(print "Part 1"(* gamma epsilon))
+(defn part1
+  [input]
+  (let [lines (str/split-lines input)
+        binary (for [pos (range (count (first lines)))]
+                 (if (ones? lines pos) 1 0))
+        gamma (binary-to-decimal binary)
+        epsilon (binary-to-decimal (invert binary))]
+    (* gamma epsilon)))
+
+(defn part2
+  [input]
+  (let [lines (str/split-lines input)
+        o2 (-> lines
+               (filter-by-frequency \1 \0)
+               binary-to-decimal)
+        co2 (-> lines
+                (filter-by-frequency \0 \1)
+                binary-to-decimal)]
+    (* o2 co2)))
+
+
+
+(print "Part 1" (part1 input))
+(print "Part 2" (part2 input))
+
