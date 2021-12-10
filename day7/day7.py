768e480e195022a96ebc80cd6e950c3ac6ea32dc
benhili <ben.hili1994@gmail.com>
2021-12-10 14:04:54 +1100

    Add day7 python solution

--- day7/day7.py
+++ day7/day7.py
@@ -1,4 +1,3 @@
-from math import floor
 from statistics import mean, median
 from functools import reduce
 
@@ -6,24 +5,28 @@ f = open("day7-example.txt", "r")
 data = [int(num) for num in f.readlines()[0].strip().split(",")]
 
 
-def triangleNumber(num):
-    return reduce(lambda a, b: a + b, range(1, num + 1))
+# Gets nth triangle number eg. 5th number = 5 + 4 + 3 + 2 + 1
+# https://en.wikipedia.org/wiki/Triangular_number
+def nth_triangle_number(num):
+    return reduce(lambda a, b: a + b, range(1, num + 1), 0)
 
 
-def useMedian(arr):
+def part1(arr):
     return reduce(lambda count, num: count + abs(num - median(arr)), arr, 0)
 
 
-def useAverage(arr):
-    avg = floor(mean(arr))
+# This works for the example but for my dataset I have to swap floor for round
+# not sure why I blame advent of code :)))
+def part2(arr):
+    avg = round(mean(arr))
 
     return reduce(
-        lambda count, num: count + triangleNumber(abs(num - avg)),
+        lambda count, num: count + nth_triangle_number(abs(num - avg)),
         arr,
         0,
     )
 
 
-print("Part 1: " + str(useMedian(data)))
-
-print("Part 2: " + str(useAverage(data)))
+if __name__ == "__main__":
+    print("Part 1: " + str(part1(data)))
+    print("Part 2: " + str(part2(data)))
---
+++ day7/test_day7.py
@@ -0,0 +1,24 @@
+import day7
+
+example = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+
+def test_nth_triangular_number():
+    assert day7.nth_triangular_number(5) == 15
+    assert day7.nth_triangular_number(6) == 21
+    assert day7.nth_triangular_number(7) == 28
+
+
+def test_part1():
+    assert day7.part1(example) == 37
+
+
+def test_part2():
+    assert day7.part2(example) == 168
+
+
+if __name__ == "__main__":
+    test_nth_triangular_number()
+    test_part1()
+    test_part2()
+    print("Everything passed")
